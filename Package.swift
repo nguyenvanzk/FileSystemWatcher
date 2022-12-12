9a14e49935c2cd0458b6ca567f9114116280f955
van.nguyen <van.nguyen@personifyinc.com>
2022-12-12 17:09:34 +0700

    Update SPM config

--- Package.swift
+++ Package.swift
@@ -3,9 +3,17 @@
 import PackageDescription
 
 let package = Package(
-    name: "FileSystemWatcher")
-
-package.dependencies = [
+    name: "FileSystemWatcher",
+    products: [
+        .library(name: "FileSystemWatcher", targets: ["FileSystemWatcher"])
+    ],
+    dependencies: [
       .package(url: "https://github.com/nguyenvanzk/inotify.git", .upToNextMajor(from: "1.0.2"))
-]
+    ],
+    targets: [
+        .target(name: "FileSystemWatcher", dependencies: [
+            .product(name: "inotify", package: "inotify"),
+        ])
+    ]
+)
 
